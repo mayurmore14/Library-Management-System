13bed9ad34209ef1603cf75e35d5b7dcf6a81fb0
mayurmore14 <mayurmore8505@gmail.com>
2024-02-21 15:36:39 +0530

    MODIFIED: Create Entity Layer

---
+++ src/main/java/com/infogalaxy/librarymanagementsystem/entity/LibraryMemberEntity.java
@@ -0,0 +1,16 @@
+package com.infogalaxy.librarymanagementsystem.entity;
+
+import lombok.Data;
+
+@Data
+public class LibraryMemberEntity {
+    private int memberid;
+
+    private String membername;
+
+    private int membermbno;
+
+    private String memberemail;
+
+    private String membercity;
+}
