7e492344510fdd35a7337049f26c7600703de0fb
mayurmore14 <mayurmore8505@gmail.com>
2024-02-27 12:07:42 +0530

    MODIFIED: Model Class for Author

---
+++ src/main/java/com/infogalaxy/librarymanagementsystem/model/AuthorModel.java
@@ -0,0 +1,13 @@
+package com.infogalaxy.librarymanagementsystem.model;
+
+import lombok.Data;
+
+@Data
+public class AuthorModel {
+
+    private int authorid;
+
+    private String authorname;
+
+    private String authorcity;
+}
